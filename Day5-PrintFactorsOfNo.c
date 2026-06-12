5653dd768db2b18eefd6a0d2497bf5145be157b1
Abhishekbxr282 <abhishekkumarbxr8744@gmail.com>
2026-06-12 12:21:32 +0530

    Update Day5-PrintFactorsOfNo.c

--- Day5-PrintFactorsOfNo.c
+++ Day5-PrintFactorsOfNo.c
@@ -1,37 +1,18 @@
+
 #include <stdio.h>
-long long getLargestPrimeFactor(long long n) {
-    long long max_prime = -1;
-
-    while (n % 2 == 0) {
-        max_prime = 2;
-        n /= 2; 
-    }
-
-    for (long long i = 3; i * i <= n; i += 2) {
-        while (n % i == 0) {
-            max_prime = i;
-            n /= i;
-        }
-    }
-
-    if (n > 2) {
-        max_prime = n;
-    }
-
-    return max_prime;
-}
 
 int main() {
-    long long num;
-
+    int num, i;
     printf("Enter a positive integer: ");
-    if (scanf("%lld", &num) != 1 || num <= 1) {
-        printf("Please enter a valid integer greater than 1.\n");
-        return 1;
-    }
+    scanf("%d", &num);
 
-    long long result = getLargestPrimeFactor(num);
-    printf("The largest prime factor of %lld is: %lld\n", num, result);
+    printf("Factors of %d are: ", num);
+    for (i = 1; i <= num; ++i) {
+    
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+    }
 
     return 0;
 }
