53ca5de0a43854eb6e931dee4b02c95727b6e578
Abhishekbxr282 <abhishekkumarbxr8744@gmail.com>
2026-06-14 11:48:28 +0530

    Update Day14-FreqOfElement.c

--- Day14-FreqOfElement.c
+++ Day14-FreqOfElement.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
-#include <limits.h>
- 
-int main()
-{
-	int arr[50], i, Size;
-	int first, second;
-	
-	printf("\n Please Enter the Number of elements in an array  :  ");
-	scanf("%d", &Size);
-	
-	printf("\n Please Enter %d elements of an Array \n", Size);
-	for (i = 0; i < Size; i++)
-	{
-		scanf("%d", &arr[i]);
+int main() {
+    int arr[100], freq[100];
+    int size, i, j, count;
+
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+
+    printf("Enter elements in array: ");
+    for(i=0; i<size; i++) {
+        scanf("%d", &arr[i]);
+        freq[i] = -1; 
     }
-	 
-	first = second = INT_MIN;  
-	   
-	for (i = 0; i < Size; i++)
-	{
-		if(arr[i] > first)
-		{
-			second = first;
-			first = arr[i];
-		}
-		else if(arr[i] > second && arr[i] < first)
-		{
-			second = arr[i];
-		}	
-	}
-	printf("\n The Largest Number in this Array =  %d", first);
-	printf("\n The Second Largest Number in this Array =  %d", second);
-	
-	return 0;
+    for(i=0; i<size; i++) {
+        count = 1;
+        for(j=i+1; j<size; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+                freq[j] = 0; 
+            }
+        }
+        if(freq[i] != 0) {
+            freq[i] = count;
+        }
+    }
+
+    printf("\nFrequency of all elements: \n");
+    for(i=0; i<size; i++) {
+        if(freq[i] != 0) {
+            printf("%d occurs %d times\n", arr[i], freq[i]);
+        }
+    }
+    return 0;
 }
+
+
 
  
