5da33c061370f66942091c91b57dac94ba23acdd
Abhishekbxr282 <abhishekkumarbxr8744@gmail.com>
2026-06-19 09:26:16 +0530

    Add files via upload

---
+++ Day19-SubtractTwoMatrices.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+    
+    int matrix1[100][100], matrix2[100][100], result[100][100];
+
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("\nEnter elements of the first matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("\nEnter elements of the second matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            result[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+
+    printf("\nResultant Matrix (First - Second):\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d\t", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
