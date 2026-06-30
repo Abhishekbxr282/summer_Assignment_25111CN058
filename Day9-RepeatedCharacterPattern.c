7bfc2052320ed62e1bac2a47c5f5bd02029e2db0
Abhishekbxr282 <abhishekkumarbxr8744@gmail.com>
2026-06-30 09:37:16 +0530

    Add files via upload

---
+++ Day9-RepeatedCharacterPattern.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int rows;
+    char ch = 'A';
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    printf("\nRepeated Character Pattern:\n");
+
+    
+    for (int i = 1; i <= rows; i++) {
+        
+     
+        for (int j = 1; j <= i; j++) {
+            printf("%c ", ch);
+        }
+        
+        ch++;
+        
+        printf("\n");
+    }
+
+    return 0;
+}
